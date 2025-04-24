d60530c974c7b8c357dd24bc0c51a0e089ca9932
forexpioneers <realforexpioneers@gmail.com>
2025-04-24 00:47:41 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/calendario.json")
+def calendario():
+    with open("dati_calendario.json", "r", encoding="utf-8") as f:
+        dati = json.load(f)
+    return jsonify(dati)
+
+if __name__ == "__main__":
+    app.run(debug=True)
